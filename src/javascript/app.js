c534392b5b68b6e6111d0ba369bd53a817c77713
John Martin <curmudgeon+git@rallydev.com>
2015-09-30 17:01:13 -0700

    Show epic

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -62,6 +62,7 @@ Ext.define("TSTimeTrackingByActualsChange", {
     },
     
     _updateData: function() {
+        var me = this;
         this.logger.log('updateData');
 
         if ( this._hasNeededDates() ) {
@@ -75,14 +76,31 @@ Ext.define("TSTimeTrackingByActualsChange", {
             }
             
             this.logger.log("From:", start_date, " to:", end_date);
-            
+            this.setLoading("Loading actuals...");
+
             this._getTasksThatChanged(start_date, end_date).then({
                 scope: this,
                 success: function(snaps) {
                     var rows = this._getTasksFromSnaps(snaps);
-                    this._addGrid(rows);
+                    this.setLoading("Gathering Related Information");
+                    
+                    this._getStoriesByOID(rows).then({
+                        scope: this,
+                        success: function(stories_by_oid) {
+                            this._updateEpicInformation(rows,stories_by_oid);
+                            
+                            this._updateOwnerInformation(rows);
+                            
+                            this._addGrid(rows); 
+                        },
+                        failure: function(msg) {
+                            Ext.Msg.alert("Problem loading ancillary data", msg);
+                        }
+                    
+                    }).always(function() { me.setLoading(false); });
+
                 }
-            });
+            }).always(function() { me.setLoading(false); });
         }
     },
     
@@ -100,7 +118,7 @@ Ext.define("TSTimeTrackingByActualsChange", {
                 {property: '_PreviousValues.Actuals', operator: 'exists', value: true},
                 {property:'Actuals',operator:'>',value: 0}
             ],
-            fetch: ['_PreviousValues.Actuals','FormattedID','Owner','Actuals','Name', this.getSetting('typeField')],
+            fetch: ['_PreviousValues.Actuals','FormattedID','Owner','Actuals','Name','WorkProduct', this.getSetting('typeField')],
             sorters: [{property:'_ValidFrom',direction:'ASC'}]
         };
         return this._loadSnapshots(config);
@@ -131,6 +149,61 @@ Ext.define("TSTimeTrackingByActualsChange", {
         return Ext.Object.getValues(row_hash);
     },
     
+    _getStoriesByOID: function(rows) {
+        var deferred = Ext.create('Deft.Deferred');
+        var workproducts = Ext.Array.pluck(rows,'WorkProduct');
+        var unique_workproducts = Ext.Array.unique(workproducts);
+        
+        console.log('work products:', unique_workproducts);
+        
+        var filter_array = Ext.Array.map(unique_workproducts, function(wp) {
+            return { property: 'ObjectID', value: wp };
+        });
+        
+        var config = {
+            filters: Rally.data.wsapi.Filter.or(filter_array),
+            model  : 'HierarchicalRequirement',
+            limit  : Infinity,
+            fetch  : ['FormattedID','Name','Feature','Parent']
+        };
+        
+        this._loadWSAPIItems(config).then({
+            success: function(stories) {
+                var stories_by_oid = {};
+                Ext.Array.each(stories, function(story){
+                    stories_by_oid[story.get('ObjectID')] = story;
+                });
+                deferred.resolve(stories_by_oid);
+            },
+            failure: function(msg) {
+                deferred.reject(msg);
+            }
+        });
+        return deferred.promise;
+    },
+    
+    _updateEpicInformation: function(rows,stories_by_oid) {
+        Ext.Array.each(rows, function(row){
+            var wp_oid = row.WorkProduct;
+            var story = stories_by_oid[wp_oid];
+            
+            console.log(story);
+            
+            if (story && story.get('Feature')) {
+                row['__epic'] = story.get('Feature').Parent;
+            } else {
+                row['__epic'] = {};
+            }
+        });
+    },
+    
+    _updateOwnerInformation: function(rows) {
+        var owners = Ext.Array.pluck(rows, 'Owner');
+        var unique_owners = Ext.Array.unique(owners);
+        
+        console.log('owners:',unique_owners);
+    },
+    
     _loadSnapshots: function(config) {
         var deferred = Ext.create('Deft.Deferred');
         var me = this;
@@ -153,11 +226,31 @@ Ext.define("TSTimeTrackingByActualsChange", {
         return deferred.promise;
     },
     
+    _loadWSAPIItems: function(config){
+        var deferred = Ext.create('Deft.Deferred');
+        var me = this;
+        
+        var default_config = {
+            fetch: ['ObjectID']
+        };
+        
+        Ext.create('Rally.data.wsapi.Store', Ext.merge(default_config,config)).load({
+            callback : function(records, operation, successful) {
+                if (successful){
+                    deferred.resolve(records);
+                } else {
+                    me.logger.log("Failed: ", operation);
+                    deferred.reject('Problem loading: ' + operation.error.errors.join('. '));
+                }
+            }
+        });
+        return deferred.promise;
+    },
+    
     _addGrid: function(rows){
         this.logger.log('_addGrid', rows);
         
         this.down('#display_box').removeAll();
-        
         var store = Ext.create('Rally.data.custom.Store',{data:rows});
         
         this.down('#display_box').add({
@@ -168,39 +261,18 @@ Ext.define("TSTimeTrackingByActualsChange", {
                 {dataIndex:'FormattedID', text:'Task Number'},
                 {dataIndex: this.getSetting('typeField'), text: 'Task Type' },
                 {dataIndex: 'Owner', text: 'Task Owner' },
-                {dataIndex:'__delta', text:'Actual Time'}
+                {dataIndex:'__delta', text:'Actual Time'},
+                {dataIndex:'__epic', text: 'Epic', renderer: function(v) {
+                    if ( !v.FormattedID ) {
+                        return "--";
+                    } 
+                    
+                    return v.FormattedID;
+                } }
             ]
         });
     },
     
-    _loadAStoreWithAPromise: function(model_name, model_fields){
-        var deferred = Ext.create('Deft.Deferred');
-        var me = this;
-        this.logger.log("Starting load:",model_name,model_fields);
-          
-        Ext.create('Rally.data.wsapi.Store', {
-            model: model_name,
-            fetch: model_fields
-        }).load({
-            callback : function(records, operation, successful) {
-                if (successful){
-                    deferred.resolve(this);
-                } else {
-                    me.logger.log("Failed: ", operation);
-                    deferred.reject('Problem loading: ' + operation.error.errors.join('. '));
-                }
-            }
-        });
-        return deferred.promise;
-    },
-    
-    _displayGrid: function(store,field_names){
-        this.down('#display_box').add({
-            xtype: 'rallygrid',
-            store: store,
-            columnCfgs: field_names
-        });
-    },
     
     getOptions: function() {
         return [
